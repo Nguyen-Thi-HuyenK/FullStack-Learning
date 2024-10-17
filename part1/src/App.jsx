233f930a7b08e15236a821f94ce65377168c966d
Huyen Nguyen <c2ngth00@students.oamk.fi>
2024-10-17 13:55:20 +0300

    new

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,19 +1,30 @@
-const Hello = () => {
+const Hello = (props) => {
+  console.log(props)
+
+  const bornYear = () => {
+    const yearNow = newDate().getFullYear()
+    return yearNow - props.age
+  }
+
   return (
     <div>
-      <p>Hello world</p>
+      <p>Hello {props.name}, you are {props.age} years old</p>
+      <p>You were probably born in {bornYear} </p>
     </div>
   )
 }
 
 
 const App = () => {
+  const name = 'Peter'
+  const age = 10
 
   return (
     <div>
       <h1>Greetings</h1>
-      <Hello/>
-      <Hello/>
+      <Hello name='Leo' age={3+4}/>
+      <Hello name='Lilian' age ='4'/>
+      <Hello name={name} age={age}/>
     </div>
   )
 }
