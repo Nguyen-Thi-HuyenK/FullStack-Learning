b51c4ec8661ee664f67fb4f039cab6baac23c04d
Huyen Nguyen <c2ngth00@students.oamk.fi>
2024-10-27 15:57:32 +0200

    add new note

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
+import { useState } from 'react';
 import Note from './components/Note';
-/* const Note = ({note}) => {
-  return (
-    <li>{note.content}</li>
-  )
-} */
 
-const App = ({notes}) => {
+
+const App = (props) => {
+  console.log(props);
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState('A new note...');
+  
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: String(notes.length + 1),
+    }
+    setNotes(notes.concat(noteObject));
+    setNewNote('');
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  }
 
   return (
     <div>
@@ -16,6 +32,12 @@ const App = ({notes}) => {
           <Note key={note.id} note = {note} />
         )}
       </ul>
+      <form onSubmit={addNote}>
+        <input 
+          value={newNote}
+          onChange={handleNoteChange} />
+        <button type='submit'>Save</button>
+      </form>
     </div>
   )
 }
