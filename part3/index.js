124034cf4ca74299057319d49a4e9de01237fbfa
Huyen Nguyen <c2ngth00@students.oamk.fi>
2024-11-07 15:30:36 +0200

    crud methods

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let notes = [
     {
       id: "1",
@@ -19,14 +21,35 @@ let notes = [
     }
   ]
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
+app.get('/api/notes/:id', (req, res) => {
+  const id = req.params.id
+  const note = notes.find(note => note.id === id)
+
+  if (note) {
+  res.json(note)
+  } else {
+    res.status(404).end()
+  }
 })
 
-app.get('/api/notes', (req, res) => {
-    res.json(notes)
-}
-)
+app.delete('/api/notes/:id', (req, res) => {
+  const id = req.params.id
+  notes = notes.filter(note => note.id !== id)
+
+  res.status(204).end()
+})
+
+app.post('/api/notes', (req, res) => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => Number(n.id)))
+    : 0
+
+    const note = req.body
+    note.id = String(maxId + 1)
+    notes = notes.concat(note)
+    res.json(note)
+})
+
 
 const PORT = 3001
 app.listen(PORT)
