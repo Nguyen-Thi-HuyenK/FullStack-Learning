9655c5550675796b945119eb8d1e94c61f83ea22
Huyen Nguyen <c2ngth00@students.oamk.fi>
2024-11-07 15:52:29 +0200

    update

--- part3/index.js
+++ part3/index.js
@@ -39,15 +39,32 @@ app.delete('/api/notes/:id', (req, res) => {
   res.status(204).end()
 })
 
-app.post('/api/notes', (req, res) => {
+const generateId = () => {
   const maxId = notes.length > 0
     ? Math.max(...notes.map(n => Number(n.id)))
     : 0
+  return String(maxId + 1)
+}
 
-    const note = req.body
-    note.id = String(maxId + 1)
-    notes = notes.concat(note)
-    res.json(note)
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+  
+
+  const note = {
+    content: body.content,
+    important: Boolean(body.important) || false,
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
 })
 
 
